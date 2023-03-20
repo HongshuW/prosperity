d651d03f965ab8a9234bdd3d9db4ef20265d5592
HongshuW <hongshu.wang.uni@gmail.com>
2023-03-20 21:46:50 +0800

    round1-strategy1

--- trader.py
+++ trader.py
@@ -2,10 +2,13 @@ from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order
 
 
-PEARLS_LIMIT = 20
-BANANA_LIMIT = 20
+SINGLE_TRADE_SIZE = 5
+LIMITS = {'PEARLS': 20, 'BANANAS': 20}
+PRICES = {'PEARLS': 10000, 'BANANAS': 5000}
 
 class Trader:
+    def __init__(self):
+        self.positions = dict()
 
     def market_making(self, product: str, order_depth: OrderDepth) -> list[Order]:
         # Initialize the list of Orders to be sent as an empty list
@@ -23,8 +26,8 @@ class Trader:
             # best_bid_volume = order_depth.buy_orders[best_bid]
         else:
             return
+        volume = SINGLE_TRADE_SIZE
         if best_bid + 1 < best_ask - 1:
-            volume = 5
             orders.append(Order(product, best_bid + 1, volume))
             orders.append(Order(product, best_ask - 1, -volume))
             print("BUY", volume, "x", best_bid + 1)
@@ -39,8 +42,14 @@ class Trader:
         # Initialize the method output dict as an empty dict
         result = {}
 
+        for product in state.position.keys():
+            self.positions[product] = state.position[product]
+
+        print(self.positions)
+
         # Iterate over all the keys (the available products) contained in the order dephts
         for product in state.order_depths.keys():
+            # if abs(state.position[product]) + SINGLE_TRADE_SIZE <= LIMITS[product]:
             order_depth: OrderDepth = state.order_depths[product]
             orders = self.market_making(product, order_depth)
             result[product] = orders
