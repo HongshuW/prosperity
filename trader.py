d327b37aa57287568ad7319f7547f5e58ef79da9
HongshuW <hongshu.wang.uni@gmail.com>
2023-03-24 00:27:35 +0800

    Implemented helper methods

--- trader.py
+++ trader.py
@@ -22,7 +22,27 @@ class Trader:
         else:
             return -1
 
-    def market_making(self, product: str, order_depth: OrderDepth) -> list[Order]:
+    def get_volume(self, product: str, is_buy: bool, position) -> int:
+        limit = LIMITS[product]
+        current_position = position[product]
+        if is_buy:
+            if current_position >= limit:
+                return 0
+            else:
+                return limit - current_position
+        else:
+            if current_position <= -limit:
+                return 0
+            else:
+                return current_position + limit
+
+    def buy_product(self, product: str, price: int, volume: int) -> Order:
+        return Order(product, price, volume)
+
+    def sell_product(self, product: str, price: int, volume: int) -> Order:
+        return Order(product, price, -volume)
+
+    def market_making(self, product: str, order_depth: OrderDepth, position) -> list[Order]:
         orders: list[Order] = []
 
         best_ask = self.get_best_ask(order_depth)
@@ -30,13 +50,17 @@ class Trader:
         if best_ask == -1 or best_bid == -1:
             return orders
 
-        volume = SINGLE_TRADE_SIZE
         if best_bid + 1 < best_ask - 1:
-            orders.append(Order(product, best_bid + 1, volume))
-            orders.append(Order(product, best_ask - 1, -volume))
-            # print("BUY", volume, "x", best_bid + 1)
-            # print("SELL", volume, "x", best_ask - 1)
-            print("-----market making: ", product, "-----")
+            if position.keys().__contains__(product):
+                volume = min(self.get_volume(product, True, position), self.get_volume(product, False, position))
+            else:
+                volume = SINGLE_TRADE_SIZE
+            if volume > 0:
+                buy_order = self.buy_product(product, best_bid + 1, volume)
+                sell_order = self.sell_product(product, best_ask - 1, volume)
+                orders.append(buy_order)
+                orders.append(sell_order)
+                print("-----market making: ", product, "-----")
         return orders
 
     def long_short_position(self, product: str, order_depth: OrderDepth) -> list[Order]:
@@ -51,7 +75,6 @@ class Trader:
                 volume = min(best_ask_volume, SINGLE_TRADE_SIZE)
                 orders.append(Order(product, best_ask, volume))
                 print("-----buy: ", product, "-----")
-                # print("BUY", volume, "x", best_ask)
 
         best_bid = self.get_best_bid(order_depth)
         if best_bid == -1:
@@ -62,39 +85,8 @@ class Trader:
                 volume = min(best_bid_volume, SINGLE_TRADE_SIZE)
                 orders.append(Order(product, best_bid, -volume))
                 print("-----sell: ", product, "-----")
-                # print("SELL", volume, "x", best_bid)
 
         return orders
-
-    def print_pol(self, own_trades, position, order_depths) -> None:
-        if len(own_trades.keys()) == 0:
-            print("\nprofit or loss: ", self.profit)
-            return
-
-        for product in own_trades.keys():
-            trades = own_trades[product]
-            for trade in trades:
-                pol = trade.price * trade.quantity
-                if trade.buyer == 'SUBMISSION':
-                    # spent money
-                    self.profit -= pol
-                elif trade.seller == 'SUBMISSION':
-                    # gained money
-                    self.profit += pol
-
-        for product in position.keys():
-            pos = position[product]
-            order_depth = order_depths[product]
-            if pos > 0:
-                # need to sell
-                price = self.get_best_bid(order_depth)
-                self.profit += price * pos
-            elif pos < 0:
-                # need to buy
-                price = self.get_best_ask(order_depth)
-                self.profit -= price * pos
-
-        print("\nprofit or loss: ", self.profit)
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
@@ -107,20 +99,16 @@ class Trader:
         own_trades = state.own_trades
         order_depths = state.order_depths
 
-        self.print_pol(own_trades, position, order_depths)
-        print("\nposition: ", position)
-
         for product in order_depths.keys():
             if position.keys().__contains__(product):
-                if abs(position[product]) + SINGLE_TRADE_SIZE <= LIMITS[product]:
-                    order_depth: OrderDepth = order_depths[product]
-                    orders = self.market_making(product, order_depth)
-                    orders += self.long_short_position(product, order_depth)
-                    result[product] = orders
+                order_depth: OrderDepth = order_depths[product]
+                orders = self.market_making(product, order_depth, position)
+                # orders += self.long_short_position(product, order_depth)
+                result[product] = orders
             else:
                 order_depth: OrderDepth = order_depths[product]
-                orders = self.market_making(product, order_depth)
-                orders += self.long_short_position(product, order_depth)
+                orders = self.market_making(product, order_depth, position)
+                # orders += self.long_short_position(product, order_depth)
                 result[product] = orders
 
         return result
